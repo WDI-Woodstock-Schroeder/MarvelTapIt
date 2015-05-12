6e816e9ad152f69b5864d6e88eee8212210776d4
Ken Barrios <keniswaiting@gmail.com>
2015-05-12 16:44:39 -0500

    andrews session controller notes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,17 @@
 class SessionsController < ApplicationController
   def log_in_behavior
+    # See if I can find the user by the username
+    # See if the password they entered is correct
+      # TRUE Place into the seesion that user's id
+      session[:user_id]
+      # Redirect to ...
+
+      # False Redirecto to ... back to login
   end
 
   def log_out_behavior
+    # replace the user id in session with nil
+    session[:user_id]
+    # redirect to ....
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,6 @@ class UsersController < ApplicationController
 
   def create
     user = User.create(user_params)
-    #redirect_to "/"
     redirect_to "/register"
 
   end
