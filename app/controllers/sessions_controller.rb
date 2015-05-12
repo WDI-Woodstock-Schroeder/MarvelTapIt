69b75ee73a496fcc5c8c2e9792e699a540d1abaf
Ken Barrios <keniswaiting@gmail.com>
2015-05-12 16:50:09 -0500

    adds complete session controller code

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,21 @@
 class SessionsController < ApplicationController
   def log_in_behavior
     # See if I can find the user by the username
+    user = User.find_by(username: params[:username])
     # See if the password they entered is correct
+    if user && user.authenticate(params[:password])
       # TRUE Place into the seesion that user's id
-      session[:user_id]
+      session[:user_id] = user.id
       # Redirect to ...
-
-      # False Redirecto to ... back to login
+    else
+      # False Redirect to ... back to login
+      redirect_to '/sign_in'
   end
 
   def log_out_behavior
     # replace the user id in session with nil
-    session[:user_id]
+    session[:user_id] = nil
     # redirect to ....
+    redirect_to '/sign_in'
   end
 end
