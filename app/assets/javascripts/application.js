f4001d0e130422a30e161ef76ecbd0ec68b5ef07
Marion Huebner <mphuebner@gmail.com>
2015-05-12 20:36:32 -0500

    adding vendor resources and javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,49 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+
+$(document).ready(function() {
+
+  $(".hero-thumb").on('click', function() {
+
+    var char = $(this).data("character");
+    console.log(char)
+
+    $.ajax({
+      method: 'get',
+      url: 'http://gateway.marvel.com:80/v1/public/characters/" + char + "?apikey=195de66a5cefd39b309c2eb0ca7463f1',
+      dataType: 'json',
+      success: function(data){
+        console.log(data);
+        name = data.data.results[0].name;
+        description = data.data.results[0].description;
+      }
+    })
+
+  });
+
+
+  $(".nemesis-thumb").on('click', function() {
+
+    var char = $(this).data("character");
+    console.log(char)
+
+    $.ajax({
+      method: 'get',
+      url: 'http://gateway.marvel.com:80/v1/public/characters/" + char + "?apikey=195de66a5cefd39b309c2eb0ca7463f1',
+      dataType: 'json',
+      success: function(data){
+        console.log(data);
+        name = data.data.results[0].name;
+        description = data.data.results[0].description;
+      }
+    })
+
+    // hide all the heroes
+    /*if (heroes and villians are hidden) {
+      //move on
+    }*/
+
+  });
+})
