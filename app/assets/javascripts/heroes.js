0235abde915a23898d3719e44551baf785a367ed
Marion Huebner <mphuebner@gmail.com>
2015-05-12 20:52:54 -0500

    image asset migration, javascript and design files migrated

--- app/assets/javascripts/heroes.js
+++ app/assets/javascripts/heroes.js
@@ -1,19 +1,19 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-
-function getFighter() {
-  $.ajax({
-    method: 'get',
-    url: 'http://gateway.marvel.com:80/v1/public/characters/1009421?apikey=195de66a5cefd39b309c2eb0ca7463f1',
-    dataType: 'json',
-    success: function(data){
-      console.log(data);
-      id = data.data.results[0].id;
-      name = data.data.results[0].name;
-      description = data.data.results[0].description;
-      path = data.data.results[0].thumbnail.path;
-      extension = data.data.results[0].thumbnail.extension;
-      thumbnail = path + "." + extension;
-    }
-  })
-};
+// 
+// function getFighter() {
+//   $.ajax({
+//     method: 'get',
+//     url: 'http://gateway.marvel.com:80/v1/public/characters/1009421?apikey=195de66a5cefd39b309c2eb0ca7463f1',
+//     dataType: 'json',
+//     success: function(data){
+//       console.log(data);
+//       id = data.data.results[0].id;
+//       name = data.data.results[0].name;
+//       description = data.data.results[0].description;
+//       path = data.data.results[0].thumbnail.path;
+//       extension = data.data.results[0].thumbnail.extension;
+//       thumbnail = path + "." + extension;
+//     }
+//   })
+// };
