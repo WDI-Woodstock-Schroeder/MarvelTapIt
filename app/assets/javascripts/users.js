a20dfc0b17e0da07a546d0e63699e6440d434fff
Marion Huebner <mphuebner@gmail.com>
2015-05-13 16:07:29 -0500

    add play button - if 2 players are selected - unstyled

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,11 +1,10 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-
-
 $(document).ready(function() {
 
-  $(".selected-hero").hide()
-  $(".selected-hero").hide()
+  $(".selected-hero").hide();
+  $(".selected-hero").hide();
+  $(".play").hide();
 
   $(".hero-thumb").on('click', function() {
 
@@ -19,16 +18,18 @@ $(document).ready(function() {
       url: 'http://gateway.marvel.com:80/v1/public/characters/' + char + '?apikey=195de66a5cefd39b309c2eb0ca7463f1',
       dataType: 'json',
       success: function(data){
-        console.log(data);
+        // console.log(data);
 
-        $(".hero-thumb").hide(700)
-        $(".selected-hero").show(700)
+        $(".hero-thumb").hide(500)
+        $(".selected-hero").show(500)
 
         name = data.data.results[0].name;
         description = data.data.results[0].description;
 
+        // remove any elements on the page with class of 'hero'
         $('.hero').remove()
 
+        // create new img element with class 'hero'
         var img = document.createElement('img');
         img.src = filename;
         img.className = "hero";
@@ -42,8 +43,8 @@ $(document).ready(function() {
 
 // show list of hero thumbnails for player reselection
   $("#reselect-hero").click(function(){
-    $(".selected-hero").hide(700);
-    $(".hero-thumb").show(700);
+    $(".selected-hero").hide(500);
+    $(".hero-thumb").show(500);
   });
 
 
@@ -62,37 +63,36 @@ $(document).ready(function() {
       url: 'http://gateway.marvel.com:80/v1/public/characters/' + char + '?apikey=195de66a5cefd39b309c2eb0ca7463f1',
       dataType: 'json',
       success: function(data){
-        console.log(data);
+        // console.log(data);
 
-        $(".nemesis-thumb").hide(700)
-        $(".selected-nemesis").show(700)
+        $(".nemesis-thumb").hide(500)
+        $(".selected-nemesis").show(500)
 
         name = data.data.results[0].name;
         description = data.data.results[0].description;
 
+        // remove any page elements with class 'nemesis'
         $('.nemesis').remove()
 
+        // create new image element with class 'nemesis'
         var img = document.createElement('img');
-
         img.src = filename;
         img.className = "nemesis";
 
         $('.selected-nemesis').prepend(img);
-
-
       }
     })
 
   // show list of nemesis thumbnails for player reselection
     $("#reselect-nemesis").click(function(){
-      $(".selected-nemesis").hide(700);
-      $(".nemesis-thumb").show(700);
+      $(".selected-nemesis").hide(500);
+      $(".nemesis-thumb").show(500);
     });
 
     // hide all the heroes
-    /*if (heroes and villians are hidden) {
-      //move on
-    }*/
+    if ($(".nemesis") && $(".hero")){
+      $(".play").show(500);
+    };
 
   });
 })
