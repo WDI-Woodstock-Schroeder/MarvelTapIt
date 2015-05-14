88499d9ef372ef0fa7fef74134c10d160b208347
Ken Barrios <keniswaiting@gmail.com>
2015-05-14 12:26:55 -0500

    successfully tests hero health bar effectiveness

--- app/assets/javascripts/play.js
+++ app/assets/javascripts/play.js
@@ -12,5 +12,15 @@ app.play = function playHandler(){
 
     // <div class="six column selected-hero"></div>
     $(".nemesis-bio").empty();
+
+    var heroDiv = '<div class="hero-health"data-total="1000" data-value="1000"><div class="bar"><div class="hit"></div></div></div><br /><button class="damage random">hit random</button><button class="reset">reset</button>'
+
+    $(".hero-bio").append(heroDiv);
+
+    var nemesisDiv = '<div class="nemesis-health"data-total="1000" data-value="1000"><div class="bar"><div class="hit"></div></div></div><br /><button class="damage random">hit random</button><button class="reset">reset</button>'
+
+    $(".nemesis-bio").append(nemesisDiv);
+
+    app.health_bar();
   })
 }
