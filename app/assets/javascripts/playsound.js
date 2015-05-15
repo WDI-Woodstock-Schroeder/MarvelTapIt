97dc536f47608f6b8a6912bbdd5ab3599735d49c
Ken Barrios <keniswaiting@gmail.com>
2015-05-15 17:16:28 -0500

    polished mvp

--- app/assets/javascripts/playsound.js
+++
@@ -1,7 +0,0 @@
-// selector: `button` with an id attribute of `playSoundBtn`
-$('#playSoundBtn').on('click', function(event) {
-
-    // get the audio element by Id and play() it
-    document.getElementById('kablam!').play();
-
-});
